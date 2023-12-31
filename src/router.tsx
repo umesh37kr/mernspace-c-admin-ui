5576d8aa9415d6d16d0e77605bbb251d1090a302
umesh <umesh37kr@gmail.com>
2023-12-31 13:39:26 +0530

    add vitest

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Catogories from "./pages/catogories";
+import Catogories from "./pages/Catogories";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
