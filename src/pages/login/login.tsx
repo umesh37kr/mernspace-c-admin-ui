54748ce5cc1153183e0d5b859474ae28969f9d84
umesh <umesh37kr@gmail.com>
2024-01-03 13:26:53 +0530

    create login functionality

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,7 +1,33 @@
-import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex } from "antd";
+import {
+  Layout,
+  Card,
+  Space,
+  Form,
+  Input,
+  Checkbox,
+  Button,
+  Flex,
+  Alert,
+} from "antd";
 import { LockFilled, LockOutlined, UserOutlined } from "@ant-design/icons";
 import Logo from "../../components/icons/Logo";
+import { useMutation } from "@tanstack/react-query";
+import { Credentials } from "../../types";
+import { login } from "../../http/api";
+
+const loginUser = async (credentials: Credentials) => {
+  // server call logic
+  const { data } = await login(credentials);
+  return data;
+};
 const LoginPage = () => {
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: loginUser,
+    onSuccess: async () => {
+      console.log("login successful");
+    },
+  });
   return (
     <>
       <Layout
@@ -25,7 +51,20 @@ const LoginPage = () => {
               </Space>
             }
           >
-            <Form initialValues={{ remember: true }}>
+            {isError && (
+              <Alert
+                style={{ marginBottom: 23 }}
+                type="error"
+                message={error?.message}
+              />
+            )}
+            <Form
+              initialValues={{ remember: true }}
+              onFinish={(values) => {
+                mutate({ email: values.username, password: values.password });
+                console.log(values);
+              }}
+            >
               <Form.Item
                 name={"username"}
                 rules={[
@@ -68,6 +107,7 @@ const LoginPage = () => {
                   type="primary"
                   htmlType="submit"
                   style={{ width: "100%" }}
+                  loading={isPending}
                 >
                   Log in
                 </Button>
