d73b9c29501d8f49651646adb12499a255b7b67a
umesh <umesh37kr@gmail.com>
2024-02-14 10:15:26 +0530

    add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,15 +1,79 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { NavLink, Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
+import Icon from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import UserIcon from "../components/icons/UserIcon";
+import { foodIcon } from "../components/icons/FoodIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+const { Sider, Header, Footer, Content } = Layout;
 
+const items = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
+  },
+  {
+    key: "/users",
+    icon: <Icon component={UserIcon} />,
+    label: <NavLink to="/users">User</NavLink>,
+  },
+  {
+    key: "/restaurants",
+    icon: <Icon component={foodIcon} />,
+    label: <NavLink to="/restaurants">Restaurants</NavLink>,
+  },
+  {
+    key: "/products",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/products">Products</NavLink>,
+  },
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+];
 const Dashboard = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   const { user } = useAuthStore();
   if (user === null) {
     return <Navigate to="/auth/login" replace={true} />;
   }
   return (
     <div>
-      <h1>Dashboard page</h1>
-      <Outlet />
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Mern Space Pizza Shop</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 };
