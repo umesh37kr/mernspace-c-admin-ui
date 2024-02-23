c1f0c339783489eb2cb62210ad025e5bb5d9e5e0
umesh <umesh37kr@gmail.com>
2024-02-23 13:00:25 +0530

    add tenant drawer functioality on button click

--- src/pages/tenants/Tenants.tsx
+++ src/pages/tenants/Tenants.tsx
@@ -1,9 +1,11 @@
 import { RightOutlined } from "@ant-design/icons";
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Button, Drawer, Space } from "antd";
 import { Link } from "react-router-dom";
 import TenantsFilter from "./TenantsFilter";
-
+import { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
 const Tenants = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
   return (
     <>
       <Breadcrumb
@@ -17,7 +19,30 @@ const Tenants = () => {
           },
         ]}
       />
-      <TenantsFilter />
+      <TenantsFilter>
+        <Button type="primary" onClick={() => setOpenDrawer(true)}>
+          <PlusOutlined /> Add Restaurants
+        </Button>
+      </TenantsFilter>
+
+      <Drawer
+        title="Add Restaurants"
+        width={720}
+        destroyOnClose={true}
+        open={openDrawer}
+        onClose={() => {
+          setOpenDrawer(false);
+        }}
+        extra={
+          <Space>
+            <Button>Cancel</Button>
+            <Button type="primary">Submit</Button>
+          </Space>
+        }
+      >
+        <p>this is sample demo</p>
+        <p>this is sample demo</p>
+      </Drawer>
     </>
   );
 };
--- src/pages/tenants/TenantsFilter.tsx
+++ src/pages/tenants/TenantsFilter.tsx
@@ -1,8 +1,12 @@
-import { PlusOutlined } from "@ant-design/icons";
-import { Button, Card, Space } from "antd";
+import { Card, Space } from "antd";
 import Search from "antd/es/input/Search";
 import type { SearchProps } from "antd/es/input/Search";
-const TenantsFilter = () => {
+import React from "react";
+
+type TenantFilterProps = {
+  children?: React.ReactNode;
+};
+const TenantsFilter = ({ children }: TenantFilterProps) => {
   const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
     console.log(info?.source, value);
   return (
@@ -16,9 +20,7 @@ const TenantsFilter = () => {
             onSearch={onSearch}
             enterButton
           />
-          <Button type="primary">
-            <PlusOutlined /> Add Restaurants
-          </Button>
+          {children}
         </Space>
       </Card>
     </>
