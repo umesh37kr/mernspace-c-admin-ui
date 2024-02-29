401ad4136666cc56e4908cac7a0112ffcb84eac2
umesh <umesh37kr@gmail.com>
2024-02-29 10:50:25 +0530

    add key in userform

--- src/pages/users/forms/UserForms.tsx
+++ src/pages/users/forms/UserForms.tsx
@@ -128,7 +128,7 @@ const UserForms = () => {
                       onChange={() => {}}
                     >
                       {tenants?.map((tenant: Tenant) => (
-                        <Select.Option value={tenant.id}>
+                        <Select.Option value={tenant.id} key={tenant.id}>
                           {tenant.name}
                         </Select.Option>
                       ))}
